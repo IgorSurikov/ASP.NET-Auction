c2c259e9db4463e1d51c54a6edbfcd9e08f7d44c
Igor Surikov <i.surikov@outlook.com>
2020-10-07 08:35:04 +0300

    status message

--- Auction/Controllers/AuctionProductLotsController.cs
+++ Auction/Controllers/AuctionProductLotsController.cs
@@ -9,22 +9,21 @@ using Auction.Data;
 using Auction.Models;
 using Auction.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.CodeAnalysis;
 
 namespace Auction.Controllers
 {
-    public class UserProductLotsController : Controller
+    public class AuctionProductLotsController : Controller
     {
         private readonly AuctionContext _context;
         private readonly UserManager<AuctionUser> _userManager;
 
-        public UserProductLotsController(AuctionContext context, UserManager<AuctionUser> userManager)
+        public AuctionProductLotsController(AuctionContext context, UserManager<AuctionUser> userManager)
         {
             _context = context;
             _userManager = userManager;
         }
 
-        // GET: UserProductLots
+        // GET: AuctionProductLots
         public async Task<IActionResult> Index()
         {
             var auctionContext1 = _context.ProductLot.Include(p => p.Customer).Include(p => p.Owner)
@@ -32,7 +31,7 @@ namespace Auction.Controllers
             return View(await auctionContext1.ToListAsync());
         }
 
-        // GET: UserProductLots/Details/5
+        // GET: AuctionProductLots/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -53,44 +52,90 @@ namespace Auction.Controllers
             return View(productLot);
         }
 
-        // GET: UserProductLots/Create
-        public IActionResult Create()
+        // GET: AuctionProductLots/Create
+
+
+        // GET: AuctionProductLots/Edit/5
+        public async Task<IActionResult> RaisePrice(int? id)
         {
-            var userId = _userManager.GetUserId(User);
-            ViewData["ProductId"] =
-                new SelectList(
-                    _context.Set<Product>().Include(p => p.ProductLot).Where(p =>
-                        p.AuctionUserId == userId && p.ProductLot == null),
-                    "ID", "ProductName");
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productLot = await _context.ProductLot.Include(p => p.Product).FirstOrDefaultAsync(p => p.ID == id);
+            if (productLot == null)
+            {
+                return NotFound();
+            }
+
+            return View(productLot);
         }
 
-        // POST: UserProductLots/Create
+        // POST: AuctionProductLots/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(
-            [Bind(
-                "ID,LotName,StartPrice,StartTrading,EndTrading,ProductId")]
-            ProductLot productLot)
+        public async Task<IActionResult> RaisePrice(int id, int newPrice)
         {
-            if (ModelState.IsValid)
+            if (id == null)
             {
-                productLot.IsActive = true;
-                productLot.CurrentPrice = productLot.StartPrice;
-                productLot.UpdateDateTime = DateTime.Now;
-                productLot.OwnerAuctionUserId = _userManager.GetUserId(User);
-                productLot.CustomerAuctionUserId = null;
-                _context.Add(productLot);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return NotFound();
             }
 
-            ViewData["ProductId"] = new SelectList(_context.Set<Product>(), "ID", "ProductDesc", productLot.ProductId);
+            var productLot = await _context.ProductLot.Include(p => p.Product).FirstOrDefaultAsync(p => p.ID == id);
+            if (productLot == null)
+            {
+                return NotFound();
+            }
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            if (newPrice >= user.Wallet)
+            {
+                ViewData["StatusMessage"] = "Error: You don't have enough money";
+                return View(productLot);
+            }
+            if (newPrice <= productLot.CurrentPrice)
+            {
+                ViewData["StatusMessage"] = "Error: New price less then current price";
+                return View(productLot);
+            }
+
+
+            /*if (id != productLot.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productLot);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductLotExists(productLot.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }*/
             return View(productLot);
         }
 
+        // GET: AuctionProductLots/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -111,13 +156,12 @@ namespace Auction.Controllers
             return View(productLot);
         }
 
-        // POST: UserProductLots/Delete/5
+        // POST: AuctionProductLots/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var productLot = await _context.ProductLot.Include(p => p.Product).FirstOrDefaultAsync(p => p.ID == id);
-            //productLot.Product.ProductLot = null;
+            var productLot = await _context.ProductLot.FindAsync(id);
             _context.ProductLot.Remove(productLot);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
