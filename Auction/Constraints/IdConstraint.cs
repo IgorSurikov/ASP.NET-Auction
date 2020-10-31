3a3aa952e1b526070256ee7beb36182451615514
Igor Surikov <isurikov@datamola.com>
2020-10-31 11:22:39 +0300

    routes

---
+++ Auction/Constraints/IdConstraint.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auction.Constraints
+{
+	public class IdConstraint : IRouteConstraint
+	{
+		public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+		{
+			if(System.Convert.ToInt32(values[routeKey]) < 0)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+}
